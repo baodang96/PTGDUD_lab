2114a33d50739e5eceb2315ab24783474454269d
H82M19\Student <Student>
2025-04-16 16:24:04 +0700

    couter app with useReducer

---
+++ Redux/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import UseReducer from './CouterApp/UseReducer'
+
+function App() {
+
+  return (
+    <>
+      <UseReducer></UseReducer>
+    </>
+  )
+}
+
+export default App
---
+++ Redux/src/CouterApp/UseReducer.jsx
@@ -0,0 +1,30 @@
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return { count: state.count + 1 };
+      case 'DECREMENT':
+        return { count: state.count - 1 };
+      case 'RESET':
+        return { count: 0 };
+      default:
+        return state;
+    }
+  };
+
+const Counter = () => {
+  const initialState = { count: 0 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <h2>Count: {state.count}</h2>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
+      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
